cc70b580adaa4f332a7c99048c286440d81e0fc3
JoseP-UR <urivesrosa@gmail.com>
2023-03-13 20:03:08 -0300

    create and get movements backend implementantion and integration

--- app/public/js/table/movementTable.js
+++ app/public/js/table/movementTable.js
@@ -1,7 +1,7 @@
 class MovementTable extends HTMLElement {
-    template = document.createElement('template');
+  template = document.createElement("template");
 
-    templateContent = `
+  templateContent = `
         <style>
             .table {
                 width: 100%;
@@ -91,160 +91,192 @@ class MovementTable extends HTMLElement {
         </table>
     `;
 
-    isEditing = false;
-    currentPage = 1;
-    pageSize = 5;
-    selectedMonth = `${this.currentMonth < 10 ? '0' + String(this.currentMonth) : this.currentMonth}/${new Date().getFullYear()}`;
+  isEditing = false;
+  currentPage = 1;
+  pageSize = 5;
+  selectedMonth = `${
+    this.currentMonth < 10 ? "0" + String(this.currentMonth) : this.currentMonth
+  }/${new Date().getFullYear()}`;
+  initialized = false;
 
-    get currentMonth() {
-        return new Date().getMonth() + 1;
-    }
+  get currentMonth() {
+    return new Date().getMonth() + 1;
+  }
 
-    get movements() {
-        return JSON.parse(localStorage.getItem('movements')) || [];
-    }
-
-    set movements(movements) {
-        localStorage.setItem('movements', JSON.stringify(movements));
-    }
-
-    get movementsByMonthAndYear() {
-        const movements = this.movements;
-        const movementsByMonthAndYear = {};
-        movements.forEach(movement => {
-            const month = movement.date.split('-')[1];
-            const year = movement.date.split('-')[0];
-            const key = `${month}/${year}`
-            if (!movementsByMonthAndYear[key]) {
-                movementsByMonthAndYear[key] = [];
-            }
-            movementsByMonthAndYear[key].push(movement);
+  get movements() {
+    if (!this.initialized) {
+      this.initialized = true;
+      localStorage.setItem("movements", JSON.stringify([]));
+      fetch("/api/movements")
+        .then((response) => response.json())
+        .then((movements) => {
+          this.movements = movements;
+          this.updateTable();
         });
-        return movementsByMonthAndYear;
+      return [];
     }
+    return JSON.parse(localStorage.getItem("movements"));
+  }
 
-    get lastId() {
-        const movements = JSON.parse(localStorage.getItem('movements'));
-        if (!movements || movements.length === 0) {
-            return 0;
-        }
-        return movements[movements.length - 1].id;
+  set movements(movements) {
+    localStorage.setItem("movements", JSON.stringify(movements));
+  }
+
+  get movementsByMonthAndYear() {
+    const movements = this.movements;
+    const movementsByMonthAndYear = {};
+    movements.forEach((movement) => {
+      const month = movement.date.split("-")[1];
+      const year = movement.date.split("-")[0];
+      const key = `${month}/${year}`;
+      if (!movementsByMonthAndYear[key]) {
+        movementsByMonthAndYear[key] = [];
+      }
+      movementsByMonthAndYear[key].push(movement);
+    });
+    return movementsByMonthAndYear;
+  }
+
+  get lastId() {
+    const movements = JSON.parse(localStorage.getItem("movements"));
+    if (!movements || movements.length === 0) {
+      return 0;
     }
+    return movements[movements.length - 1].id;
+  }
 
-    get total() {
-        return this.movementsByMonthAndYear[this.selectedMonth].reduce((acc, movement) => parseFloat(acc) + parseFloat(movement.amount), 0);
-    }
+  get total() {
+    const selectedMonth = this.movementsByMonthAndYear[this.selectedMonth]
+      ? this.selectedMonth
+      : Object.keys(this.movementsByMonthAndYear)[0];
+    return this.movementsByMonthAndYear[selectedMonth].reduce(
+      (acc, movement) => parseFloat(acc) + parseFloat(movement.amount),
+      0
+    );
+  }
 
-    get table() {
-        return this.shadowRoot.querySelector('table');
-    }
+  get table() {
+    return this.shadowRoot.querySelector("table");
+  }
 
-    get tbody() {
-        return this.table.querySelector('tbody');
-    }
+  get tbody() {
+    return this.table.querySelector("tbody");
+  }
 
-    constructor() {
-        super();
-        this.attachShadow({ mode: 'open' });
-        this.template.innerHTML = this.templateContent;
-        this.shadowRoot.appendChild(this.template.content.cloneNode(true));
-    }
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.template.innerHTML = this.templateContent;
+    this.shadowRoot.appendChild(this.template.content.cloneNode(true));
+  }
 
-    connectedCallback() {
-        this.updateTable();
-        this.table.querySelector('#prevPageBtn').addEventListener('click', this.handlePrevPage);
-        this.table.querySelector('#nextPageBtn').addEventListener('click', this.handleNextPage);
-        this.table.querySelector('#downloadBtn').addEventListener('click', this.handleDownload);
-        this.table.querySelector('#loadBtn').addEventListener('click', this.handleLoad);
-    }
+  connectedCallback() {
+    this.updateTable();
+    this.table
+      .querySelector("#prevPageBtn")
+      .addEventListener("click", this.handlePrevPage);
+    this.table
+      .querySelector("#nextPageBtn")
+      .addEventListener("click", this.handleNextPage);
+    this.table
+      .querySelector("#downloadBtn")
+      .addEventListener("click", this.handleDownload);
+    this.table
+      .querySelector("#loadBtn")
+      .addEventListener("click", this.handleLoad);
+  }
 
-    handleDownload = () => {
-        //downloads all movements in a csv file
-        const movements = this.movements;
-        let csvContent = "data:text/csv;charset=utf-8,";
-        movements.forEach(movement => {
-            const row = `${movement.id},${movement.description},${movement.amount},${movement.date},${movement.category},${movement.to},${movement.from}`;
-            csvContent += row + "\r\n";
+  handleDownload = () => {
+    //downloads all movements in a csv file
+    const movements = this.movements;
+    let csvContent = "data:text/csv;charset=utf-8,";
+    movements.forEach((movement) => {
+      const row = `${movement.id},${movement.description},${movement.amount},${movement.date},${movement.category},${movement.to},${movement.from}`;
+      csvContent += row + "\r\n";
+    });
+    const encodedUri = encodeURI(csvContent);
+    const link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "movements.csv");
+    document.body.appendChild(link);
+    link.click();
+  };
+
+  handleLoad = () => {
+    const input = document.createElement("input");
+    input.type = "file";
+    input.accept = ".csv";
+    input.onchange = (e) => {
+      const file = e.target.files[0];
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const csv = e.target.result;
+        const movements = [];
+        const rows = csv.split("\r\n");
+        rows.forEach((row) => {
+          const movement = row.split(",");
+          if (!movement[0]) {
+            return;
+          }
+          movements.push({
+            id: parseInt(movement[0]),
+            description: movement[1],
+            amount: parseFloat(movement[2]),
+            date: movement[3],
+            category: movement[4],
+            to: movement[5],
+            from: movement[6],
+          });
         });
-        const encodedUri = encodeURI(csvContent);
-        const link = document.createElement("a");
-        link.setAttribute("href", encodedUri);
-        link.setAttribute("download", "movements.csv");
-        document.body.appendChild(link);
-        link.click();
-    }
-
-    handleLoad = () => {
-        const input = document.createElement('input');
-        input.type = 'file';
-        input.accept = '.csv';
-        input.onchange = (e) => {
-            const file = e.target.files[0];
-            const reader = new FileReader();
-            reader.onload = (e) => {
-                const csv = e.target.result;
-                const movements = [];
-                const rows = csv.split("\r\n");
-                rows.forEach(row => {
-                    const movement = row.split(',');
-                    if (!movement[0]) {
-                        return;
-                    }
-                    movements.push({
-                        id: parseInt(movement[0]),
-                        description: movement[1],
-                        amount: parseFloat(movement[2]),
-                        date: movement[3],
-                        category: movement[4],
-                        to: movement[5],
-                        from: movement[6]
-                    });
-                });
-                this.movements = movements;
-                this.updateTable();
-            };
-            reader.readAsText(file);
-        }
-        input.click();
-    }
-
-    handlePrevPage = () => {
-        if (this.currentPage === 1) {
-            return;
-        }
-        this.currentPage--;
+        this.movements = movements;
         this.updateTable();
-    }
+      };
+      reader.readAsText(file);
+    };
+    input.click();
+  };
 
-    handleNextPage = () => {
-        if (this.currentPage === Math.ceil(this.movements.length / this.pageSize)) {
-            return;
-        }
-        this.currentPage++;
-        this.updateTable();
+  handlePrevPage = () => {
+    if (this.currentPage === 1) {
+      return;
     }
+    this.currentPage--;
+    this.updateTable();
+  };
 
-    getActionButtons(id) {
-        return `
+  handleNextPage = () => {
+    if (this.currentPage === Math.ceil(this.movements.length / this.pageSize)) {
+      return;
+    }
+    this.currentPage++;
+    this.updateTable();
+  };
+
+  getActionButtons(id) {
+    return `
             <button class="editBtn" data-id="${id}">Edit</button>
             <button class="deleteBtn" data-id="${id}">Delete</button>
         `;
-    }
+  }
 
-    updateTable() {
-        this.tbody.innerHTML = '';
-        const monthMovements = this.movementsByMonthAndYear[this.selectedMonth];
-        if (!monthMovements || monthMovements.length === 0) {
-            const tr = document.createElement('tr');
-            tr.innerHTML = ''
-            this.tbody.appendChild(tr);
-        return;
-        }
-        const pageMovements = this.movementsByMonthAndYear[this.selectedMonth].slice((this.currentPage - 1) * this.pageSize, this.currentPage * this.pageSize);
-        pageMovements.forEach(movement => {
-            const tr = document.createElement('tr');
-            tr.id = `movement-${movement.id}`;
-            tr.innerHTML = `
+  updateTable() {
+    this.tbody.innerHTML = "";
+    let selectedMonth = this.selectedMonth;
+    const monthMovements = this.movementsByMonthAndYear[selectedMonth];
+    if (!monthMovements || monthMovements.length === 0) {
+      selectedMonth = Object.keys(this.movementsByMonthAndYear)[0];
+    }
+    if (!this.movementsByMonthAndYear[selectedMonth]) {
+      return;
+    }
+    const pageMovements = this.movementsByMonthAndYear[selectedMonth].slice(
+      (this.currentPage - 1) * this.pageSize,
+      this.currentPage * this.pageSize
+    );
+    pageMovements.forEach((movement) => {
+      const tr = document.createElement("tr");
+      tr.id = `movement-${movement.id}`;
+      tr.innerHTML = `
                 <td>${movement.id}</td>
                 <td>${movement.description}</td>
                 <td>${movement.amount}</td>
@@ -254,47 +286,52 @@ class MovementTable extends HTMLElement {
                 <td>${movement.from}</td>
                 <td>${this.getActionButtons(movement.id)}</td>
             `;
-            this.tbody.appendChild(tr);
-            tr.querySelector('.deleteBtn').addEventListener('click', this.handleDelete);
-            tr.querySelector('.editBtn').addEventListener('click', this.handleEdit);
-        });
-        this.table.querySelector('#movementTotal').innerHTML = `${this.total}`;
-        this.table.querySelector('#currentPage').innerHTML = `${this.currentPage}`;
-        this.table.querySelector('#totalPages').innerHTML = `${Math.ceil(this.movements.length / this.pageSize)}`;
-        this.renderMonthsTabs();
-    }
+      this.tbody.appendChild(tr);
+      tr.querySelector(".deleteBtn").addEventListener(
+        "click",
+        this.handleDelete
+      );
+      tr.querySelector(".editBtn").addEventListener("click", this.handleEdit);
+    });
+    this.table.querySelector("#movementTotal").innerHTML = `${this.total}`;
+    this.table.querySelector("#currentPage").innerHTML = `${this.currentPage}`;
+    this.table.querySelector("#totalPages").innerHTML = `${Math.ceil(
+      this.movementsByMonthAndYear[selectedMonth].length / this.pageSize
+    )}`;
+    this.renderMonthsTabs();
+  }
 
-    renderMonthsTabs() {
-        const tabNames = Object.keys(this.movementsByMonthAndYear);
-        const tabButtons = tabNames.map(tabName => {
-            const button = document.createElement('button');
-            button.value = tabName;
-            button.innerHTML = tabName;
-            return button;
-        });
-        this.table.querySelector('#monthTabs td').innerHTML = `
+  renderMonthsTabs() {
+    const tabNames = Object.keys(this.movementsByMonthAndYear);
+    const tabButtons = tabNames.map((tabName) => {
+      const button = document.createElement("button");
+      button.value = tabName;
+      button.innerHTML = tabName;
+      return button;
+    });
+    this.table.querySelector("#monthTabs td").innerHTML = `
         Months:
-                ${tabButtons.map(button => button.outerHTML).join('')}
+                ${tabButtons.map((button) => button.outerHTML).join("")}
         `;
-        this.table.querySelectorAll('#monthTabs button').forEach(button => {
-            button.addEventListener('click', this.handleMonthTabClick);
-        });
-    }
+    this.table.querySelectorAll("#monthTabs button").forEach((button) => {
+      button.addEventListener("click", this.handleMonthTabClick);
+    });
+  }
 
-    handleMonthTabClick = (e) => {
-        this.selectedMonth = e.target.value;
-        this.currentPage = 1;
-        this.updateTable();
-    }
+  handleMonthTabClick = (e) => {
+    this.selectedMonth = e.target.value;
+    this.currentPage = 1;
+    this.updateTable();
+  };
 
-    handleEdit = (e) => {
-        if (this.isEditing) {
-            return;
-        }
-        this.isEditing = true;
-        const id = e.target.dataset.id;
-        const movement = this.movements.find(movement => movement.id == id);
-        this.tbody.querySelector(`#movement-${id}`).innerHTML = `
+  handleEdit = (e) => {
+    if (this.isEditing) {
+      return;
+    }
+    this.isEditing = true;
+    const id = e.target.dataset.id;
+    const movement = this.movements.find((movement) => movement.id == id);
+    this.tbody.querySelector(`#movement-${id}`).innerHTML = `
             <td>${movement.id}</td>
             <td><input type="text" value="${movement.description}"></td>
             <td><input type="number" value="${movement.amount}"></td>
@@ -304,46 +341,56 @@ class MovementTable extends HTMLElement {
             <td><input type="text" value="${movement.from}"></td>
             <td><button id="doneEditBtn" data-id="${movement.id}">Done</button></td>
         `;
-        this.tbody.querySelector('#doneEditBtn').addEventListener('click', this.handleDoneEdit);
+    this.tbody
+      .querySelector("#doneEditBtn")
+      .addEventListener("click", this.handleDoneEdit);
+  };
+
+  handleDoneEdit = (e) => {
+    const id = e.target.dataset.id;
+    //creates new object so we can manipulate the instance
+    const movements = this.movements;
+    const movement = movements.find((movement) => movement.id == id);
+    const inputs =
+      e.target.parentElement.parentElement.querySelectorAll("input");
+
+    movement.description = inputs[0].value;
+    movement.amount = inputs[1].value;
+    movement.date = inputs[2].value;
+    movement.category = inputs[3].value;
+    movement.to = inputs[4].value;
+    movement.from = inputs[5].value;
+
+    this.movements = movements;
+    this.updateTable();
+    this.isEditing = false;
+  };
+
+  handleDelete = (e) => {
+    const id = e.target.dataset.id;
+    this.movements = this.movements.filter((movement) => movement.id != id);
+    if (!this.movementsByMonthAndYear[this.selectedMonth]) {
+      const months = Object.keys(this.movementsByMonthAndYear);
+      this.selectedMonth = months[months.length - 1];
     }
+    this.updateTable();
+  };
 
-    handleDoneEdit = (e) => {
-        const id = e.target.dataset.id;
-        //creates new object so we can manipulate the instance
-        const movements = this.movements;
-        const movement = movements.find(movement => movement.id == id);
-        const inputs = e.target.parentElement.parentElement.querySelectorAll('input');
-
-        movement.description = inputs[0].value;
-        movement.amount = inputs[1].value;
-        movement.date = inputs[2].value;
-        movement.category = inputs[3].value;
-        movement.to = inputs[4].value;
-        movement.from = inputs[5].value;
-
-        this.movements = movements;
-        this.updateTable();
-        this.isEditing = false;
-    }
-
-
-    handleDelete = (e) => {
-        const id = e.target.dataset.id;
-        this.movements = this.movements.filter(movement => movement.id != id);
-        if (!this.movementsByMonthAndYear[this.selectedMonth]) {
-            const months = Object.keys(this.movementsByMonthAndYear);
-            this.selectedMonth = months[months.length - 1];
-        }
-        this.updateTable();
-    }
-
-    handleAddMovement = ({detail: data}) => {
-        data.id = this.lastId + 1;
-        const movements = this.movements;
-        movements.push(data);
-        this.movements = movements;
-        this.updateTable();
-    }
+  handleAddMovement = async ({ detail: data }) => {
+    data.id = this.lastId + 1;
+    fetch("/api/movements", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then(async (res) => {
+      const movements = this.movements;
+      movements.push(await res.json());
+      this.movements = movements;
+      this.updateTable();
+    });
+  };
 }
 
-customElements.define('movement-table', MovementTable);
+customElements.define("movement-table", MovementTable);
--- app/src/main.js
+++ app/src/main.js
@@ -1,17 +1,43 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
+const express = require("express");
+const path = require("path");
+const mongodb = require("mongodb");
+const bodyParser = require("body-parser");
 
 const app = express();
+const mongoUser = "root";
+const mongoPassword = "root";
+const mongoUrl = `mongodb://${mongoUser}:${mongoPassword}@mongodb:27017`;
+const client = new mongodb.MongoClient(mongoUrl);
 
-app.get('/js/*', (req, res) => {
-    res.header('Content-Type', 'text/javascript');
-    res.sendFile(path.join(__dirname, '../public', req.url));
+app.use(bodyParser.json());
+
+app.get("/js/*", (req, res) => {
+  res.header("Content-Type", "text/javascript");
+  res.sendFile(path.join(__dirname, "../public", req.url));
 });
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.get("/api/movements", async (req, res) => {
+  const db = client.db("finances");
+  const collection = db.collection("movements");
+  res.json(await collection.find({}).toArray());
+});
+
+app.post("/api/movements", async (req, res) => {
+  console.log(req.body);
+  const db = client.db("finances");
+  const collection = db.collection("movements");
+  const result = await collection.insertOne(req.body);
+  res.json(
+    await collection.findOne({
+      _id: result.insertedId,
+    })
+  );
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
